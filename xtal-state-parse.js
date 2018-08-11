58fb768bf6a2aee4ba20b095de23460dbba98a1f
bahrus <anderson.bruce.b@gmail.com>
2018-08-11 13:41:08 -0400

    initial implementtion of xtal-state-parse

---
+++ xtal-state-parse.js
@@ -0,0 +1,37 @@
+import { XtallatX } from 'xtal-latx/xtal-latx.js';
+const with_url_pattern = 'with-url-pattern';
+export class XtalStateParse extends XtallatX(HTMLElement) {
+    static get is() { return 'xtal-state-parse'; }
+    static get observedAttributes() { return super.observedAttributes.concat([with_url_pattern]); }
+    attributeChangedCallback(name, oldVal, newVal) {
+        super.attributeChangedCallback(name, oldVal, newVal);
+        switch (name) {
+            case with_url_pattern:
+                this._withURLPattern = newVal;
+                break;
+        }
+        this.onPropsChange();
+    }
+    get withURLPattern() {
+        return this._withURLPattern;
+    }
+    set withURLPattern(val) {
+        this.attr(with_url_pattern, val);
+    }
+    connectedCallback() {
+        this._upgradeProperties(['disabled', 'withURLPattern']);
+        this._isConnected = true;
+        this.onPropsChange();
+    }
+    onPropsChange() {
+        if (!this._isConnected || this._disabled || !this._withURLPattern || history.state)
+            return;
+        const reg = new RegExp(this._withURLPattern);
+        const parsed = reg.exec(location.href);
+        if (!parsed)
+            return;
+        history.replaceState(parsed['groups'], 'Init', location.pathname);
+    }
+}
+customElements.define(XtalStateParse.is, XtalStateParse);
+//# sourceMappingURL=xtal-state-parse.js.map
