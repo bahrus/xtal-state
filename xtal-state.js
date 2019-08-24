c5db862964fee8395006a0bfa99bf1020324efd0
bahrus <anderson.bruce.b@gmail.com>
2019-08-24 09:28:49 -0400

    ...

--- xtal-state.js
+++ xtal-state.js
@@ -1,6 +1,4 @@
 import './StoreKeeper.js';
-import './xtal-state-api.js';
 import './xtal-state-parse.js';
 import './xtal-state-watch.js';
-import './xtal-state-commit.js';
 import './xtal-state-update.js';
