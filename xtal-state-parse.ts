6e2bf0c79c25cbb3f20ec7937ecfa453da20218f
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-07-03 19:19:20 -0400

    ...

--- xtal-state-parse.ts
+++ xtal-state-parse.ts
@@ -2,6 +2,38 @@ import {XtalStateBase} from './xtal-state-base.js';
 import {define, de} from 'xtal-element/xtal-latx.js';
 import {AttributeProps} from 'xtal-element/types.d.js';
 
+const linkValue = ({disabled, value, noMatch, _xlConnected, self, withUrlPattern, initHistoryIfNull}: XtalStateParse) => {
+    //value only gets set once
+    if(disabled || value !== undefined || noMatch || !_xlConnected) return;
+    if(!self._checkedNull){
+        if(window.history.state === null){
+            self.dataset.historyWasNull = 'true';
+        }
+        self._checkedNull = true;
+    }
+    let val: any = null;
+    if(withUrlPattern !== undefined){
+        val = self.parseAddressBar(self.parse, withUrlPattern, window);
+        if(val === -1){
+            if(!self.parseFn) return;
+            const prseString = self.getObj(self.parse, window);
+            val = self.parseFn(prseString, self);
+        }
+    }
+    if(val === null) {
+        self.noMatch = true;
+        self[de]('no-match-found', {
+            value: true,
+        }, true);
+        return;
+    }else{
+        self.value = val;
+        self[de]('match-found', {
+            value: val
+        }, true);
+    }
+    if(initHistoryIfNull && (window.history.state === null)) window.history.replaceState(val, '', window.location.href);  
+}
 
 /**
  * @element xtal-state-parse
@@ -43,39 +75,8 @@ export class XtalStateParse extends XtalStateBase{
     noMatch: boolean;
 
     _checkedNull: boolean = false;
-    onPropsChange(name: string){
-        super.onPropsChange(name);
-        if(this.disabled || this.value || this.noMatch || !this._xlConnected) return;
-        if(!this._checkedNull){
-            if(window.history.state === null){
-                this.dataset.historyWasNull = 'true';
-            }
-            this._checkedNull = true;
-        }
 
-        let value: any = null;
-        if(this.withUrlPattern){
-            value = this.parseAddressBar(this.parse, this.withUrlPattern, window);
-            if(value === -1){
-                if(!this.parseFn) return;
-                const prseString = this.getObj(this.parse, window);
-                value = this.parseFn(prseString, this);
-            }
-        }
-        if(value === null) {
-            this.noMatch = true;
-            this[de]('no-match-found', {
-                value: true,
-            }, true);
-            return;
-        }else{
-            this.value = value;
-            this[de]('match-found', {
-                value: value
-            }, true);
-        }
-        if(this.initHistoryIfNull && (window.history.state === null)) window.history.replaceState(value, '', window.location.href);
-    }
+    propActions = [linkValue];
 
     parseAddressBar(parsePath: string, urlPattern: string, winObj: Window){
         try{
